79e560fe67945ff72c92737651486ef81ac33841
wx <1104610748@qq.com>
2023-08-31 17:48:22 +0800

    更新

---
+++ deploy/build.sh
@@ -0,0 +1,14 @@
+# 确保脚本抛出遇到的错误
+set -e &&
+
+# 生成静态文件
+npm run docs:build &&
+rm -r docs/* &&
+cd main/.vitepress/dist &&
+mv * ../../../docs/
+cd ../../../ &&
+rm -r main/.vitepress/dist
+git add -A &&
+git commit -m '更新' &&
+git push -u origin master &&
+cd -
